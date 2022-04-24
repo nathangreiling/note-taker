2f85f16d58c84029ff1598f11781654d06e63839
nathangreiling <ngreiling20@gmail.com>
2022-04-24 18:22:13 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const express = require("express");
+const { notes } = require('./db/db')
+const express = require('express');
 
 const app = express();
 
+app.get('/api/db', (req, res) => {
+    res.send('Hello');
+})
+
 app.listen(3001, () => {
-    console.log(`Server now on port 3001!`)
+    console.log(`server live 3001`)
 });
