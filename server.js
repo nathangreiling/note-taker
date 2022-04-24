508d5fad7ca76a2e105a712bb31317824eaefb74
nathangreiling <ngreiling20@gmail.com>
2022-04-24 18:29:21 -0500

    solving app error

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const { notes } = require('./db/db')
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/api/db', (req, res) => {
     res.send('Hello');
 })
 
-app.listen(3001, () => {
-    console.log(`server live 3001`)
+app.listen(PORT, () => {
+    console.log(`Server now on ${PORT}!`);
 });
